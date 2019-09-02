c48f80fca159a90f6fca3d0ca8420166bef18b20
John Zarate <git@jcharante.com>
2019-09-02 13:33:34 -0400

    convert to q-item

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,5 +1,9 @@
 <template>
-    <p>{{data.name}}</p>
+    <q-item>
+        <q-item-section>
+            <q-item-label>{{data.name}}</q-item-label>
+        </q-item-section>
+    </q-item>
 </template>
 
 <script>
