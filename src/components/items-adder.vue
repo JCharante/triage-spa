37dd63e63edf51eec7467b291174635f0efcc614
John Zarate <git@jcharante.com>
2019-09-03 21:22:38 -0400

    alternative way to get enter to work

--- src/components/items-adder.vue
+++ src/components/items-adder.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <q-btn color="primary" label="Add Item" @click="createDiag"/>
-        <q-dialog v-model="prompt" @hide="removeListener" persistent>
+        <q-btn color="primary" label="Add Item" @click="prompt = true"/>
+        <q-dialog v-model="prompt" persistent>
             <q-card style="min-width: 400px">
                 <q-card-section>
                     <div class="text-h6">Item Name</div>
                 </q-card-section>
 
                 <q-card-section>
-                    <q-input dense v-model="name" autofocus @keyup.enter="prompt = false" />
+                    <q-input dense v-model="name" autofocus @keyup.enter="submit(), prompt = false" />
                 </q-card-section>
 
                 <q-card-actions align="right" class="text-primary">
@@ -30,19 +30,6 @@
             };
         },
         methods: {
-            catchEnterKey(evt) {
-                if (evt.which === 13 || evt.keyCode === 13) {
-                    this.submit();
-                    this.prompt = false;
-                }
-            },
-            createDiag() {
-                this.prompt = true;
-                window.addEventListener('keyup', this.catchEnterKey);
-            },
-            removeListener() {
-                window.removeEventListener('keyup', this.catchEnterKey);
-            },
             submit() {
                 this.$store.dispatch('createItem', this.name).then(() => {
                     this.name = '';
