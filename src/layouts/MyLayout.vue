9409dfc7a534e41b16a4d82285d718b792f300ee
jcharante <git@jcharante.com>
2019-09-09 07:33:14 -0400

    remove drawer

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -2,16 +2,6 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-        >
-          <q-icon name="menu" />
-        </q-btn>
-
         <q-toolbar-title>
           Triage
         </q-toolbar-title>
@@ -20,28 +10,6 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-2"
-    >
-      <q-list>
-        <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable to="/">
-          <q-item-section>
-            <q-item-label>Docs</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable to="/about">
-          <q-item-section>
-            <q-item-label>About</q-item-label>
-            <q-item-label caption>How Triage was built</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -49,18 +17,8 @@
 </template>
 
 <script>
-    import { openURL } from 'quasar';
-
     export default {
         name: 'MyLayout',
-        data() {
-            return {
-                leftDrawerOpen: false,
-            };
-        },
-        methods: {
-            openURL,
-        },
     };
 </script>
 
